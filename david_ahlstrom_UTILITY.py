359d65ce4033941b2af1ffb9bbf4f02a1f38db8c
David J Ahlstrom <77700800+davidahlstrom@users.noreply.github.com>
2022-11-22 20:06:38 -0500

    Update david_ahlstrom_UTILITY.py

--- david_ahlstrom_UTILITY.py
+++ david_ahlstrom_UTILITY.py
@@ -2,7 +2,7 @@ import random, string
 
 def create_account_no():
     '''
-    creates a rendom 4 digit alphanumeric account number for 
+    creates a random 4 digit alphanumeric account number for 
     '''
     
     number = ''.join(random.choices(string.ascii_letters.upper() + string.digits, k=4))
