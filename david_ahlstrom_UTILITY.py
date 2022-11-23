e65950bc30ad11e1008aa00663b58d965300d1d9
David J Ahlstrom <77700800+davidahlstrom@users.noreply.github.com>
2022-11-22 20:06:20 -0500

    Update david_ahlstrom_UTILITY.py

--- david_ahlstrom_UTILITY.py
+++ david_ahlstrom_UTILITY.py
@@ -1,20 +1,3 @@
-# -------------------------------------------------------------------------------
-# Final Project: Banking Application
-# Name: David Ahlstrom
-# Python Version:  3.7.8
-#-------------------------------------------------------------------------------
-# Honor Code Statement: I received no assistance on this assignment that
-#                       violates the ethical guidelines as set forth by the
-#                       instructor and the class syllabus.
-#-------------------------------------------------------------------------------
-# References: Slides and example code in weekly content
-#-------------------------------------------------------------------------------
-# Comments to grader: N/A
-#-------------------------------------------------------------------------------
-# Code:
-#-------------------------------------------------------------------------------
-
-
 import random, string
 
 def create_account_no():
